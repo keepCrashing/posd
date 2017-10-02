dde819669d5f770350050b46608f47ec5dd12367
keepCrushing <deer.of.nine.colors@gmail.com>
2017-10-02 23:47:35 +0800

    no message

--- Atom.cpp
+++ Atom.cpp
@@ -1,7 +1,6 @@
 #include "include/Atom.h"
 #include "include/Number.h"
 #include "include/Variable.h"
-#include <string>
-using std::string;
+
 Atom::Atom (string s):_symbol(s) {}
 bool Atom::operator ==(Atom a) {return _symbol == a._symbol;}
